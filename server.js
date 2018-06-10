725f32dbbcf52b586389d32f848f5d3cd0ef5e11
Glitch (fcc-exercise-tracker-ajb) <none>
2018-06-10 15:52:15 +0000

    💵👕 Checkpoint ./server.js:1080052/3499 ./views/index.html:1080052/68

--- server.js
+++ server.js
@@ -1,22 +1,108 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-
-const cors = require('cors')
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+// init project
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      cors = require('cors'),
+      mongodb = require('mongodb').MongoClient(),
+      mongoose = require('mongoose'),
+      bluebird = require('bluebird'),
+      shortid = require('shortid'),
+      moment = require('moment'),
+      path = require('path'),
+      dotenv = require('dotenv').config(),
+      port = process.env.PORT || 8080;
 
 app.use(cors())
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+mongoose.Promise = require('bluebird');
+mongoose.connect(process.env.MLAB_URI)
+const db = mongoose.connection;
 
-
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+db.once('open', () => {
+  console.log('Connected to MongoDB');
 });
+
+db.on('error', () => {
+  console.log('Unable to connect to MongoDB. Error');  
+});
+
+const userInfo = require('./models/userDB')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post("/api/exercise/new-user", (req,res) => {
+  let newUsername = req.body.username.toString()
+  let userid = shortid.generate()
+  let newUser = new userInfo({
+    username: req.body.username, 
+    userid: userid,
+    exercise: [], 
+    count: 0
+  })
+  console.log('created new user ' +newUser)
+    
+  userInfo.findOne({"username": newUsername}, (err, user) => {
+    
+    if(user === "" || undefined){
+      res.json({error: "username required"})
+    }
+    
+    else if(err) {
+      console.log(err);
+      return res.send('error: searching existing users');
+    }
+    else if(user!==null) {
+      return res.send('username exists already')
+    }
+    newUser.save((err, user) => {
+      if(err) throw err
+      res.json({"username": newUsername,
+      "userid": userid})
+    }); //end of newUser save
+    console.log(newUser)
+  }); //end of findOne function 
+}); //end of app.post for new user
+
+app.post("/api/exercise/add", (req, res) => {
+  
+  //let user = req.body.userId
+  userInfo.findOne({"userid": req.body.userId}, (err, user) => {
+    console.log(req.body.userId)
+    if(err) throw err
+    user.exercise.unshift({              
+      "description": req.body.description,  
+      "duration": req.body.duration,
+      "date": req.body.date
+    })
+    
+    let newCount = Number(user.count) + Number(req.body.duration);
+    user.Count = newCount;
+    user.save((err, user) => {
+      if (err) throw err; 
+    });
+    res.json(user);
+  }); //end of findOne
+}); //end of app.post
+
+
+app.get("/api/exercise/log", (req, res) => {
+  let user = req.query.getuserid;
+  console.log(user);
+  
+  userInfo.findOne({"userid": user}, (err, user) => {
+    if(err) {
+      console.log(err);
+      return res.send('error: searching existing users');
+    }
+    res.json(user)  
+   
+ });
+})
 
 
 // Not found middleware
